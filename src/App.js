d4f5e14462f68387399169632ccef2138400ba7a
Kajal Saini <sizukabts91@gmail.com>
2025-08-21 21:19:52 +0530

    Add Background

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-
+import Background from './components/Background';
 import Navigationbar from './components/Navigationbar';
 import './App.css'
 import React, { useState } from 'react';
@@ -8,6 +8,7 @@ function App() {
     <div className="App">
       <Navigationbar darkMode={darkMode}
       setDarkMode={setDarkMode}/>
+      <Background />
     </div>
   );
 }
